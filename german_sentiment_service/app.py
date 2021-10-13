7ef2c8c275c6be31f1c02744d20da65a7f197d09
tomrodenhagen <rodenhag@gmail.com>
2021-10-13 12:27:00 +0200

    Add git ignore

--- german_sentiment_service/app.py
+++ german_sentiment_service/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, APIRouter
 from germansentiment import SentimentModel
 
+#We only want to load this once
 sentiment_model = SentimentModel()
 
 from views import api_router
