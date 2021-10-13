9d6b9d9ff64dcc10a495a4b330ff357a5f831b36
tomrodenhagen <rodenhag@gmail.com>
2021-10-13 12:12:01 +0200

    Add real classifier

--- german_sentiment_service/service.py
+++ german_sentiment_service/service.py
@@ -2,5 +2,6 @@ class SentimentService:
     def __init__(self, model):
         self.model = model
 
-    def get_sentiment(self, texts):
-        return self.model.predict_sentiment(texts)
+    def get_sentiment(self, text):
+        batch = [text]
+        return self.model.predict_sentiment(batch)[0]
--- german_sentiment_service/views.py
+++ german_sentiment_service/views.py
@@ -4,10 +4,7 @@ from app_service import get_sentiment_service
 
 api_router = APIRouter()
 
-
 @api_router.post('/', status_code=201)
-async def get_sentiment(sentiment_service: SentimentService = Depends(get_sentiment_service)):
-   # texts = payload.dict()
-  #  result = sentiment_service.get_sentiment(texts)
-
-    return {'result': "SENTIMENT"}
+async def get_sentiment(text :str , sentiment_service: SentimentService = Depends(get_sentiment_service)):
+    result = sentiment_service.get_sentiment(text)
+    return {'sentiment': result}
